974c6f6660c2d12831a4decb3d97b87ec57f0acd
jndan21 <118275197+Eschatos21xx@users.noreply.github.com>
2025-10-13 17:25:31 +0700

    Add files via upload

---
+++ PHP/tipe_data.php
@@ -0,0 +1,12 @@
+<?php
+
+$nama = "Kamal Jindan Alfharizie";
+$umur = 18;
+$tinggi = 165;
+$aktif = true;
+
+echo "Nama: $nama<br>";
+echo "Umur: $umur<br>";
+echo "Tinggi: $tinggi<br>";
+echo "Status aktif: $aktif<br>";
+?>
