da02236768a6b03a7685dd491f10b6cec54c45f0
jndan21 <118275197+Eschatos21xx@users.noreply.github.com>
2025-10-13 17:06:07 +0700

    Add files via upload

---
+++ C++/tipe_data.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+string nama = "Kamal Jindan Alfharizie";
+int umur = 18;
+float tinggi = 165;
+bool aktif = true;
+
+    cout << "Nama: " << nama << endl;
+    cout << "Umur: " << umur << endl;
+    cout << "Tinggi: " << tinggi << endl;
+    cout << "Status aktif: " << aktif<< endl;
+
+    system("pause");
+    return 0;
+    
+}
